8680f4ba026a5c248bbd8210262060b2a0ca0359
xjs20071001 <34676792@qq.com>
2017-05-04 15:15:13 +0800

    Initial commit

---
+++ src/com/xjs/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xjs.demo;
+
+/**
+ * Created by Administrator on 2017/4/21 0021.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
